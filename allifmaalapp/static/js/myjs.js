ae5ceaf9346d82a89be66a51b4c95b2ab241582d
Ahmed Muse <ahmeddove114@gmail.com>
2022-09-30 21:05:43 +0300

    test

--- allifmaalapp/static/js/myjs.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  
-    $(".datetimeinput").datepicker({changeYear: false,changeMonth: true, dateFormat: 'yy-mm-dd'});
-   
-
-    $('.table').paging({limit:3});
-  });
--- allifproject/urls.py
+++ allifproject/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('myfristapp', include('myfirstapp.urls')),
-     path('', include('allifmaalapp.urls')),
+     path('', include('myfirstapp.urls')),
+    #path('', include('allifmaalapp.urls')),
      
 ]
  
